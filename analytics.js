a24e5f3a0aed9b71aaf92726bda73388eff7ae91
Lyubov Lobacheva <kotoko666.rm@gmail.com>
2022-04-08 10:27:25 +0400

    Подключили и проверили скрипты

---
+++ analytics.js
@@ -0,0 +1,21 @@
+function createAnalytics()
+{
+    let count = 0
+    let isDestroyed = false
+    const listener = () => count++
+    document.addEventListener('click', listener)
+
+    return {
+        destroy() {
+            document.removeEventListener('click', listener)
+            isDestroyed = true
+        }, 
+        getClicks() {
+            if (isDestroyed) {
+                return 'Analytics is destroyed'
+            }
+            return count
+        }
+    }
+}
+window.analyticsss = createAnalytics()
