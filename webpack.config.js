79e98bde1d1cf3dcbf4fdea35343e2a1717b4443
Lyubov Lobacheva <kotoko666.rm@gmail.com>
2022-04-12 15:37:12 +0400

    Создали паттерн для имен бандлов

--- webpack.config.js
+++ webpack.config.js
@@ -3,9 +3,12 @@ const path = require('path')
 /** @type {import('webpack').Configuration} */
 module.exports = {
     mode: 'development',
-    entry: './src/index.js', //входная точка приложения, откуда вебпаку начать
+    entry: {
+        main: './src/index.js',
+        analytics: './src/analytics.js'
+    }, //входная точка приложения, откуда вебпаку начать
     output: {
-        filename: 'bundle.js',//когда вебпак соберет все js-скрипты, получим 1 файл bundle.js
+        filename: '[name].bundle.js',//когда вебпак соберет все js-скрипты, получим 1 файл bundle.js
         path: path.resolve(__dirname, 'dist') //Вебпак будет складывать в папку дист в текущей директории бандл
     }
 
