71ee8155cfdef15d4efbd0cacab46d27a3b1ffe6
Lyubov Lobacheva <kotoko666.rm@gmail.com>
2022-04-12 16:26:20 +0400

    Перенесли контент сюда

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,7 @@
 const path = require('path')
+//подключаем плагины
+const HTMLWebpackPlugin = require('html-webpack-plugin')
+const {CleanWebpackPlugin} = require('clean-webpack-plugin')
 
 /** @type {import('webpack').Configuration} */
 module.exports = {
@@ -8,9 +11,15 @@ module.exports = {
         analytics: './src/analytics.js'
     }, //входная точка приложения, откуда вебпаку начать
     output: {
-        filename: '[name].bundle.js',//когда вебпак соберет все js-скрипты, получим 1 файл bundle.js
+        filename: '[name].[contenthash].js',//когда вебпак соберет все js-скрипты, получим файлы bundle с именами от хэшей
         path: path.resolve(__dirname, 'dist') //Вебпак будет складывать в папку дист в текущей директории бандл
-    }
+    },
+    plugins: [
+        new HTMLWebpackPlugin({
+            template: 'src/index.html'
+        }),
+        new CleanWebpackPlugin()
+    ]
 
 }
 //Выше написана минимальная конфигурация для webpack
