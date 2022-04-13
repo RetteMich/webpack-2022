856c312e9e23a7a64235ec8b7ab615d6207930cf
Lyubov Lobacheva <kotoko666.rm@gmail.com>
2022-04-13 17:02:07 +0400

    Добавлены импорты css, json, png

--- src/index.js
+++ src/index.js
@@ -1,4 +1,9 @@
 import Post from "./Post"
-const post = new Post('Webpack Post Title')
+import './styles/styles.css' //импорт стилей
+import json from './assets/json.json'//
+import WebpackLogo from './assets/webpack-logo.png'
+const post = new Post('Webpack Post Title', WebpackLogo)
 
 console.log('Post to string: ', post.toString())
+
+console.log('JSON: ', json)
