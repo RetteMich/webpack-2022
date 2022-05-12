bd660bf71a7563b905ead43dd5562e73f187f781
Lyubov Lobacheva <kotoko666.rm@gmail.com>
2022-05-12 15:12:55 +0400

    Add scss loader

--- webpack.config.js
+++ webpack.config.js
@@ -83,7 +83,19 @@ module.exports = {
             {
                 test: /\.(png|jpg|svg|gif)$/,
                 type: 'asset/resource'
+            },
+            {
+               test: /\.s[ac]ss$/i,
+                use: [
+                    // Creates `style` nodes from JS strings
+                    "style-loader",
+                    // Translates CSS into CommonJS
+                    "css-loader",
+                    // Compiles Sass to CSS
+                    "sass-loader",
+                    ],
             }
+
         ]
             
     }
