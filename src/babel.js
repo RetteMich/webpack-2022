79c24f1e092f0d1e3eb3a3c9ddcb6b7a1255b171
Lyubov Lobacheva <kotoko666.rm@gmail.com>
2022-05-17 13:30:12 +0400

    class Util

--- src/babel.js
+++ src/babel.js
@@ -3,13 +3,9 @@ async function start() {
 }
 
 start().then(console.log)
-<<<<<<< HEAD
-=======
 
 
 class Util {
   static id = Date.now()
-
 }
 console.log('Util id:', Util.id)
->>>>>>> e64b91f3f81f3d550be856830357d1ec9616daf4
