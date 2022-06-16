8644b989d2507def308800249e5297a9e5523ef5
Lyubov Lobacheva <kotoko666.rm@gmail.com>
2022-06-16 23:28:58 +0400

    added counter and button dynamics

--- src/blocks/like/like.js
+++ src/blocks/like/like.js
@@ -1,1 +1,15 @@
-let counter;
+const likes = document.querySelectorAll(".like");
+let likeCounter = 0;
+
+likes.forEach((like) => {
+  like.addEventListener("click", () => {
+    likeCounter = like.querySelector(".like__counter");
+    if (like.classList.contains("like-pressed")) {
+      like.classList.remove("like-pressed");
+      likeCounter.textContent = +likeCounter.textContent - 1;
+    } else {
+      like.classList.add("like-pressed");
+      likeCounter.textContent = +likeCounter.textContent + 1;
+    }
+  });
+});
