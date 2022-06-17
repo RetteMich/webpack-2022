9c90dd6fb9c762581e4d653a8833bcb98447b85d
Lyubov Lobacheva <kotoko666.rm@gmail.com>
2022-06-17 16:15:19 +0400

    refactoring like block

--- src/blocks/like/like.js
+++ src/blocks/like/like.js
@@ -1,15 +1,19 @@
 const likes = document.querySelectorAll(".like");
 let likeCounter = 0;
+let likeIcon = ''
 
 likes.forEach((like) => {
   like.addEventListener("click", () => {
     likeCounter = like.querySelector(".like__counter");
-    if (like.classList.contains("like-pressed")) {
-      like.classList.remove("like-pressed");
+    likeIcon = like.querySelector(".like__icon")
+    if (like.classList.contains("like_pressed")) {
+      like.classList.remove("like_pressed");
       likeCounter.textContent = +likeCounter.textContent - 1;
+      likeIcon.textContent = 'favorite_border'
     } else {
-      like.classList.add("like-pressed");
+      like.classList.add("like_pressed");
       likeCounter.textContent = +likeCounter.textContent + 1;
+      likeIcon.textContent = 'favorite'
     }
   });
-});
+})
