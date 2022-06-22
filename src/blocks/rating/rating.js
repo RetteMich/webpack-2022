bb8824bf9739026eb2f630a87137bf0679ab6939
Lyubov Lobacheva <kotoko666.rm@gmail.com>
2022-06-22 21:23:03 +0400

    rating completed

--- src/blocks/rating/rating.js
+++ src/blocks/rating/rating.js
@@ -5,67 +5,58 @@ const ratingSettings = {
     startPosition: 0,
 };
 const ratingBox = document.querySelectorAll(".rating");
-//const stars = rating.document.querySelectorAll(".star");
 
 ratingBox.forEach((rating) => {
-    // Перебирает все, вызывает функцию для каждого.
     createRating(rating);
 });
 
 function createRating(rating) {
+    const star = document.createElement("div");
+    star.classList.add("star", "material-icons");
+    star.innerHTML = "star_border"; //star
     for (let i = 0; i < ratingSettings.size; i++) {
         const star = document.createElement("div");
         star.classList.add("star", "material-icons");
         star.innerHTML = "star_border"; //star
         rating.append(star);
     }
-
+    listenStars(rating);
+}
+function listenStars(rating) {
     const stars = rating.querySelectorAll(".star");
-    console.log(stars);
-    // stars.addEventListener("click", (event) => {
-    //     setRating(stars, [...rating.children].indexOf(event.target));
-    // });
+    stars.forEach((star) => {
+        star.addEventListener("mousemove", (event) => {
+            if (!rating.classList.contains("fixed"))
+                setRating(rating, [...rating.children].indexOf(event.target));
+        });
+        star.addEventListener("mouseleave", (event) => {
+            if (!rating.classList.contains("fixed")) clearRating(rating);
+        });
+        star.addEventListener("click", (event) => {
+            if (rating.classList.contains("fixed")) {
+                rating.classList.remove("fixed");
+            } else {
+                rating.classList.add("fixed");
+                setRating(rating, [...rating.children].indexOf(event.target));
+            }
+        });
+    });
 }
 
-// ratingBox.forEach((rating) => {
-//     for (let i = 0; i < ratingSettings.size; i++) {
-//         const star = document.createElement("div");
-//         star.classList.add("star", "material-icons");
-//         star.innerHTML = "star_border"; //star
-//         rating.append(star);
-
-//         rating.addEventListener("click", () => {
-//             star.addEventListener("click", (event) => {
-//                 setRating(rating, [...rating.children].indexOf(event.target));
-//             });
-//         });
-//     }
-// });
-
-// ratingBox.forEach((rating) => {
-//     createRating(rating);
-
-//     // rating.addEventListener("click", (event) => {
-//     //     if (event.target.classList.contains("star")) {
-//     //         setRating(rating);
-//     //     }
-//     // });
-// });
-
-// function setRating(rating) {
-//     let score = [...rating.children].indexOf(event.target);
-//     let stars = document.querySelectorAll(".star");
-//     for (let i = 0; i < score + 1; i++) {
-//         stars[i].innerHTML = "star";
-//     }
-// }
-
-function setRating(stars, index) {
+function setRating(rating, index) {
+    let stars = rating.querySelectorAll(".star");
     for (let i = 0; i < stars.length; i++) {
         if (i < index + 1) {
             stars[i].innerHTML = "star";
         } else {
             stars[i].innerHTML = "star_border";
         }
+    }
+}
+
+function clearRating(rating) {
+    let stars = rating.querySelectorAll(".star");
+    for (let i = 0; i < stars.length; i++) {
+        stars[i].innerHTML = "star_border";
     }
 }
