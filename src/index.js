af48f014395addadd38f345cb22e5e8cd3c9acdc
Lyubov Lobacheva <kotoko666.rm@gmail.com>
2022-06-23 17:29:48 +0400

    refactoring

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
-import "./main.scss";
-import "./blocks/like/like.js";
-import "./blocks/rating/rating.js";
-import "./blocks/slider/slider.js";
+import './main.scss';
+import './blocks/like/like.js';
+import './blocks/rating/rating.js';
+import './blocks/slider/slider.js';
+import './blocks/button/button.js';
 //import "./blocks/rating/jquery.star.rating.js";
