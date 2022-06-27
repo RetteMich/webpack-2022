10ae367f1be828fa5c90d055baba0e3a0edc0deb
Lyubov Lobacheva <kotoko666.rm@gmail.com>
2022-06-27 14:08:43 +0400

    import slider plugin

--- src/index.js
+++ src/index.js
@@ -3,4 +3,5 @@ import './blocks/like/like.js';
 import './blocks/rating/rating.js';
 import './blocks/slider/slider.js';
 import './blocks/button/button.js';
+import 'jquery-ui/ui/widgets/slider';
 //import "./blocks/rating/jquery.star.rating.js";
