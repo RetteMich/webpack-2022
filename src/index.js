754aada609405ab5834c1f0096c3cc634536e5ba
Lyubov Lobacheva <kotoko666.rm@gmail.com>
2022-06-27 17:27:22 +0400

    added paginstion

--- src/index.js
+++ src/index.js
@@ -3,5 +3,6 @@ import './blocks/like/like.js';
 import './blocks/rating/rating.js';
 import './blocks/slider/slider.js';
 import './blocks/button/button.js';
+import './blocks/pagination/pagination.js';
 import 'jquery-ui/ui/widgets/slider';
 //import "./blocks/rating/jquery.star.rating.js";
