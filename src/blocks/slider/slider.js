ef8e82827cb94288e38705f1534534bd631bbe6a
Lyubov Lobacheva <kotoko666.rm@gmail.com>
2022-06-27 12:18:54 +0400

    refactoring

--- src/blocks/slider/slider.js
+++ src/blocks/slider/slider.js
@@ -1,4 +1,4 @@
-$.slider = require("jquery-ui/ui/widgets/slider");
+$.slider = require('jquery-ui/ui/widgets/slider');
 
 // var $grid = $(".our-design__gallery-items").isotope({
 //   itemSelector: ".our-design__gallery-item",
@@ -6,19 +6,19 @@ $.slider = require("jquery-ui/ui/widgets/slider");
 // });
 
 $(function () {
-  $("#slider-range").slider({
-    range: true,
-    min: 0,
-    max: 500,
-    values: [75, 300],
-    slide: function (event, ui) {
-      $("#amount").val("$" + ui.values[0] + " - $" + ui.values[1]);
-    },
-  });
-  $("#amount").val(
-    "$" +
-      $("#slider-range").slider("values", 0) +
-      " - $" +
-      $("#slider-range").slider("values", 1)
-  );
+    $('.slider__body').slider({
+        range: true,
+        min: 0,
+        max: 50000,
+        values: [5000, 10000],
+        slide: function (event, ui) {
+            $('.slider__range').val(ui.values[0] + '₽ - ' + ui.values[1] + '₽');
+        },
+    });
+    $('.slider__range').val(
+        $('.slider__body').slider('values', 0) +
+            '₽ - ' +
+            $('.slider__body').slider('values', 1) +
+            '₽'
+    );
 });
