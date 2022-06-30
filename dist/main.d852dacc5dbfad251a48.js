e689960cd37531fa4dc3bb62ca5488366d4020ef
Lyubov Lobacheva <kotoko666.rm@gmail.com>
2022-06-30 14:15:11 +0400

    remove comments

---
+++ dist/main.d852dacc5dbfad251a48.js
@@ -0,0 +1,32 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./index.js":
+/*!******************!*\
+  !*** ./index.js ***!
+  \******************/
+/***/ (() => {
+
+eval("throw new Error(\"Module build failed (from ../node_modules/babel-loader/lib/index.js):\\nSyntaxError: /home/rette/my/MetaLamp-education/src/index.js: Unexpected token (6:0)\\n\\n\\u001b[0m \\u001b[90m 4 |\\u001b[39m \\u001b[36mimport\\u001b[39m \\u001b[32m'./blocks/slider/slider.js'\\u001b[39m\\u001b[33m;\\u001b[39m\\u001b[0m\\n\\u001b[0m \\u001b[90m 5 |\\u001b[39m \\u001b[36mimport\\u001b[39m \\u001b[32m'./blocks/button/button.js'\\u001b[39m\\u001b[33m;\\u001b[39m\\u001b[0m\\n\\u001b[0m\\u001b[31m\\u001b[1m>\\u001b[22m\\u001b[39m\\u001b[90m 6 |\\u001b[39m \\u001b[33m<<\\u001b[39m\\u001b[33m<<\\u001b[39m\\u001b[33m<<\\u001b[39m\\u001b[33m<\\u001b[39m \\u001b[33mHEAD\\u001b[39m\\u001b[0m\\n\\u001b[0m \\u001b[90m   |\\u001b[39m \\u001b[31m\\u001b[1m^\\u001b[22m\\u001b[39m\\u001b[0m\\n\\u001b[0m \\u001b[90m 7 |\\u001b[39m \\u001b[36mimport\\u001b[39m \\u001b[32m'./blocks/dropdown/dropdown'\\u001b[39m\\u001b[33m;\\u001b[39m\\u001b[0m\\n\\u001b[0m \\u001b[90m 8 |\\u001b[39m \\u001b[36mimport\\u001b[39m \\u001b[32m'./blocks/pagination/pagination.js'\\u001b[39m\\u001b[33m;\\u001b[39m\\u001b[0m\\n\\u001b[0m \\u001b[90m 9 |\\u001b[39m \\u001b[36mimport\\u001b[39m \\u001b[32m'./blocks/checkbox-list/checkbox-list.js'\\u001b[39m\\u001b[33m;\\u001b[39m\\u001b[0m\\n    at instantiate (/home/rette/my/MetaLamp-education/node_modules/@babel/parser/lib/index.js:72:32)\\n    at constructor (/home/rette/my/MetaLamp-education/node_modules/@babel/parser/lib/index.js:358:12)\\n    at Parser.raise (/home/rette/my/MetaLamp-education/node_modules/@babel/parser/lib/index.js:3335:19)\\n    at Parser.unexpected (/home/rette/my/MetaLamp-education/node_modules/@babel/parser/lib/index.js:3373:16)\\n    at Parser.parseExprAtom (/home/rette/my/MetaLamp-education/node_modules/@babel/parser/lib/index.js:13015:22)\\n    at Parser.parseExprSubscripts (/home/rette/my/MetaLamp-education/node_modules/@babel/parser/lib/index.js:12540:23)\\n    at Parser.parseUpdate (/home/rette/my/MetaLamp-education/node_modules/@babel/parser/lib/index.js:12519:21)\\n    at Parser.parseMaybeUnary (/home/rette/my/MetaLamp-education/node_modules/@babel/parser/lib/index.js:12490:23)\\n    at Parser.parseMaybeUnaryOrPrivate (/home/rette/my/MetaLamp-education/node_modules/@babel/parser/lib/index.js:12284:61)\\n    at Parser.parseExprOps (/home/rette/my/MetaLamp-education/node_modules/@babel/parser/lib/index.js:12291:23)\");\n\n//# sourceURL=webpack:///./index.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module doesn't tell about it's top-level declarations so it can't be inlined
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./index.js"]();
+/******/ 	
+/******/ })()
+;
