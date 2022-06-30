33032add3df64822d33fc9a0b21d96e87963a186
Lyubov Lobacheva <kotoko666.rm@gmail.com>
2022-06-30 14:15:11 +0400

    remove comments

--- src/index.js
+++ src/index.js
@@ -3,5 +3,3 @@ import './blocks/like/like.js';
 import './blocks/rating/rating.js';
 import './blocks/slider/slider.js';
 import './blocks/button/button.js';
-import 'jquery-ui/ui/widgets/slider';
-//import "./blocks/rating/jquery.star.rating.js";
